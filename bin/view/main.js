56ac71d4265b8d984e80689aba11048a526be0d3
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2022-09-05 01:02:17 +0900

    link keyword

--- bin/view/main.js
+++ bin/view/main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     lastArticles(10);
+    linkKeywords();
 }
 
 function lastArticles(num) {
@@ -11,5 +12,19 @@ function lastArticles(num) {
     }
     var url = "/last_articles.cgi?num=" + num;
     httpReq.open("GET", url, true);
+    httpReq.send(null);
+}
+
+
+function linkKeywords(){
+    var httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function(){
+        if(httpReq.readyState != 4 || httpReq.status != 200)
+            return;
+        document.getElementById("keywords").innerHTML = httpReq.responseText;
+    }
+    var word = document.getElementById("keywords").innerHTML;
+    var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
+    httpReq.open("GET",url,true);
     httpReq.send(null);
 }
