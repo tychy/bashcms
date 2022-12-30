105f3ef0db6d047415af385a391b02b4c16f4026
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2022-12-31 00:27:14 +0900

    gen keyword list

--- bin/view/main.js
+++ bin/view/main.js
@@ -15,16 +15,15 @@ function lastArticles(num) {
     httpReq.send(null);
 }
 
-
-function linkKeywords(){
+function linkKeywords() {
     var httpReq = new XMLHttpRequest();
     httpReq.onreadystatechange = function(){
-        if(httpReq.readyState != 4 || httpReq.status != 200)
-            return;
-        document.getElementById("keywords").innerHTML = httpReq.responseText;
+    if(httpReq.readyState != 4 || httpReq.status != 200)
+        return;
+    document.getElementById("keywords").innerHTML = httpReq.responseText;
     }
     var word = document.getElementById("keywords").innerHTML;
     var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
-    httpReq.open("GET",url,true);
+    httpReq.open("GET", url, true);
     httpReq.send(null);
 }
