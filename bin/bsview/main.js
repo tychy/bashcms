70219aeae4fda4d29d2a42046732a9175d303abb
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2023-01-06 18:58:56 +0900

    search

--- bin/bsview/main.js
+++ bin/bsview/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
     lastArticles(10);
     rankArticles(10);
     linkKeywords();
-    fullSearch("");
+//    fullSearch("");
 }
 
 function lastArticles(num) {
@@ -42,15 +42,19 @@ function linkKeywords() {
     httpReq.send(null);
 }
 
-function fullSearch(word) {
+function fullSearch() {
+    var word = document.getElementById("full-search-box").value;
+    if(word == "")
+        return;
+
     var httpReq = new XMLHttpRequest();
     httpReq.onreadystatechange = function(){
     if(httpReq.readyState != 4 || httpReq.status != 200)
         return;
-    document.getElementById("full-search").innerHTML = httpReq.responseText;
+    document.getElementById("article-body").innerHTML = httpReq.responseText;
     document.body.style.cursor = "default";
     }
-    var url = "/full_search.cgi?word=" + encodeURIComponent(word);
+    var url = "/bsview/full_search.cgi?word=" + encodeURIComponent(word);
     httpReq.open("GET", url, true);
     httpReq.send(null);
     document.body.style.cursor = "wait";
